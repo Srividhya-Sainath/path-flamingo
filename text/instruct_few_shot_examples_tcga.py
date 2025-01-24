2269826de3acc6d75ee37e4bde5be943a28a5126
Srividhya Sainath <83634591+Srividhya-Sainath@users.noreply.github.com>
2025-01-24 12:19:56 +0000

    Create instruct_few_shot_examples_tcga.py

---
+++ text/instruct_few_shot_examples_tcga.py
@@ -0,0 +1,8 @@
+fs = [{'conversations': [{'from': 'human',
+                          'value': 'Can you provide an overall description of the whole slide image (WSI)?'},
+                          {'from': 'gpt',
+                          'value': 'The whole slide image reveals malignant epithelial cells infiltrating the surrounding stroma with minimal glandular structures. The normal tissue architecture is significantly disrupted. The tumor cells are poorly differentiated, displaying large variability in nuclear size and shape, indicative of substantial nuclear pleomorphism. A moderate rate of cell division is observed among the tumor cells. There is no evident vascular invasion, necrosis, or microcalcifications. The intraductal component is less than 25% with notable cancerization present'},
+                        ]
+                          
+    }
+]
