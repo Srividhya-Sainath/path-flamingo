e99321f194ea56d3df6054e08cdb0b307c952919
Srividhya Sainath <83634591+Srividhya-Sainath@users.noreply.github.com>
2025-01-24 12:23:25 +0000

    Create regional_structure_few_shot_examples_tcga.py

---
+++ text/regional_structure_few_shot_examples_tcga.py
@@ -0,0 +1,8 @@
+fs = [{'conversations': [{'from': 'human',
+                          'value': 'What observations can you make about the tissue architecture on this slide?'},
+                          {'from': 'gpt',
+                          'value': 'The tissue architecture is significantly disrupted, marked by minimal formation of glandular structures. The poorly differentiated state results in a loss of the typical organized glandular architecture, and instead, cells are haphazardly arranged. There is less than 25% of the slide showing an intraductal component, and this area exhibits prominent cancerization of lobules.'},
+                        ]
+                          
+    }
+]
