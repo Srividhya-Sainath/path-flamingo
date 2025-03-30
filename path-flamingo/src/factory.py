b77274b3c002d90205fafa888eda856116b96b95
Srividhya Sainath <83634591+Srividhya-Sainath@users.noreply.github.com>
2025-03-30 17:21:01 +0200

    Added biogptforcausallm

--- path-flamingo/src/factory.py
+++ path-flamingo/src/factory.py
@@ -1,10 +1,138 @@
 from typing import Optional
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModel,AutoModelForCausalLM, AutoTokenizer
+import open_clip
 
 from .flamingo import Flamingo
 from .flamingo_lm import FlamingoLMMixin
 from .utils import extend_instance
+
+## ISKI ZAROORAT NAHI HAI ABHI
+# def create_model_and_transforms(
+#     clip_vision_encoder_path: str,
+#     clip_vision_encoder_pretrained: str,
+#     lang_encoder_path: str,
+#     tokenizer_path: str,
+#     vision_encoder_type: str = "openclip",
+#     cross_attn_every_n_layers: int = 1,
+#     use_local_files: bool = False,
+#     decoder_layers_attr_name: str = None,
+#     freeze_lm_embeddings: bool = False,
+#     cache_dir: Optional[str] = None,
+#     **flamingo_kwargs,
+# ):
+#     """
+#     Initialize a Flamingo model from a pretrained vision encoder and language encoder.
+#     Appends special tokens to the tokenizer and freezes backbones.
+
+#     Args:
+#         clip_vision_encoder_path (str): path to pretrained clip model (e.g. "ViT-B-32")
+#         clip_vision_encoder_pretrained (str): name of pretraining dataset for clip model (e.g. "laion2b_s32b_b79k")
+#         lang_encoder_path (str): path to pretrained language encoder
+#         tokenizer_path (str): path to pretrained tokenizer
+#         vision_encoder_type (str): Type of vision encoder to use ("openclip" or "titan")
+#         cross_attn_every_n_layers (int, optional): determines how often to add a cross-attention layer. Defaults to 1.
+#         use_local_files (bool, optional): whether to use local files. Defaults to False.
+#         decoder_layers_attr_name (str, optional): name of the decoder layers attribute. Defaults to None.
+#         freeze_lm_embeddings (bool, optional): whether to freeze LM input embeddings when configuring Perceiver.
+#         cache_dir (str, optional): path to cache directory for downloading OpenClip/HF weights.
+#     Returns:
+#         Flamingo: Flamingo model from pretrained vision and language encoders
+#         Image processor: Pipeline to preprocess input images
+#         Tokenizer: A tokenizer for the language model
+#     """
+#     image_processor = None
+#     if vision_encoder_type.lower() == "titan":
+#         # Initialize TITAN using AutoModel
+#         vision_encoder = AutoModel.from_pretrained(
+#             clip_vision_encoder_path,
+#             trust_remote_code=True,
+#             cache_dir=cache_dir,
+#         )
+#         print("TITAN model initialized and returned as the processor.")
+#     elif vision_encoder_type.lower() == "openclip":
+#         # Initialize OpenCLIP
+#         vision_encoder, _, image_processor = open_clip.create_model_and_transforms(
+#             clip_vision_encoder_path,
+#             pretrained=clip_vision_encoder_pretrained,
+#             cache_dir=cache_dir,
+#         )
+#         vision_encoder.visual.output_tokens = True
+#         print("OpenCLIP initialized with an image processor.")
+#     else:
+#         raise ValueError(f"Unsupported vision encoder type: {vision_encoder_type}")
+
+#     text_tokenizer = AutoTokenizer.from_pretrained(
+#         tokenizer_path,
+#         local_files_only=use_local_files,
+#         trust_remote_code=True,
+#         cache_dir=cache_dir,
+#     )
+#     # add Flamingo special tokens to the tokenizer
+#     text_tokenizer.add_special_tokens(
+#         {"additional_special_tokens": ["<|endofchunk|>", "<image>"]}
+#     )
+#     if text_tokenizer.pad_token is None:
+#         # Issue: GPT models don't have a pad token, which we use to
+#         # modify labels for the loss.
+#         text_tokenizer.add_special_tokens({"pad_token": "<PAD>"})
+
+#     lang_encoder = AutoModelForCausalLM.from_pretrained(
+#         lang_encoder_path,
+#         local_files_only=use_local_files,
+#         trust_remote_code=True,
+#         cache_dir=cache_dir,
+#     )
+
+#     # hacks for MPT-1B, which doesn't have a get_input_embeddings method
+#     if "mpt-1b-redpajama-200b" in lang_encoder_path:
+
+#         class EmbeddingFnMixin:
+#             def get_input_embeddings(self):
+#                 return self.transformer.wte
+
+#             def set_input_embeddings(self, new_embeddings):
+#                 self.transformer.wte = new_embeddings
+
+#         extend_instance(lang_encoder, EmbeddingFnMixin)
+
+#     # convert LM to FlamingoLM
+#     extend_instance(lang_encoder, FlamingoLMMixin)
+
+#     if decoder_layers_attr_name is None:
+#         decoder_layers_attr_name = _infer_decoder_layers_attr_name(lang_encoder)
+#     lang_encoder.set_decoder_layers_attr_name(decoder_layers_attr_name)
+#     lang_encoder.resize_token_embeddings(len(text_tokenizer))
+
+#     model = Flamingo(
+#         vision_encoder,
+#         lang_encoder,
+#         text_tokenizer.encode("<|endofchunk|>")[-1],
+#         text_tokenizer.encode("<image>")[-1],
+#         vis_dim=open_clip.get_model_config(clip_vision_encoder_path)["vision_cfg"][
+#             "width"
+#         ],
+#         cross_attn_every_n_layers=cross_attn_every_n_layers,
+#         **flamingo_kwargs,
+#     )
+
+#     # Freeze all parameters
+#     model.requires_grad_(False)
+#     assert sum(p.numel() for p in model.parameters() if p.requires_grad) == 0
+
+#     # Unfreeze perceiver, gated_cross_attn_layers, and LM input embeddings
+#     model.perceiver.requires_grad_(True)
+#     model.lang_encoder.gated_cross_attn_layers.requires_grad_(True)
+#     if not freeze_lm_embeddings:
+#         model.lang_encoder.get_input_embeddings().requires_grad_(True)
+#         # TODO: investigate also training the output embeddings when untied
+
+#     print(
+#         f"Flamingo model initialized with {sum(p.numel() for p in model.parameters() if p.requires_grad)} trainable parameters"
+#     )
+
+#     return model, image_processor, text_tokenizer
+
 
 def create_model_and_transforms(
     #clip_vision_encoder_path: str, ## ISKI ZAROORAT NAHI HAI
@@ -88,10 +216,14 @@ def create_model_and_transforms(
     lang_encoder.resize_token_embeddings(len(text_tokenizer)) ## After adding special tokens (<PAD>, <|endofchunk|>, <image>), the tokenizer’s vocabulary size increases, and the embedding layer must be resized accordingly.
 
     model = Flamingo(
+        #vision_encoder,
         lang_encoder,
         text_tokenizer.encode("<|endofchunk|>")[-1],
         text_tokenizer.encode("<image>")[-1],
         vis_dim= 768,
+        # vis_dim=open_clip.get_model_config(clip_vision_encoder_path)["vision_cfg"][
+        #     "width"
+        # ],
         cross_attn_every_n_layers=cross_attn_every_n_layers,
         **flamingo_kwargs,
     )
@@ -133,4 +265,5 @@ __KNOWN_DECODER_LAYERS_ATTR_NAMES = {
     "gptneoxforcausallm": "gpt_neox.layers",
     "mpt": "transformer.blocks",
     "mosaicgpt": "transformer.blocks",
+    "biogptforcausallm": "biogpt.layers",
 }
